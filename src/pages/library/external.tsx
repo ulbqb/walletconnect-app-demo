da46a8ab9036ca3ff98efffb0918a8531c321da1
Shogo Hyodo <mmoshg8u@gmail.com>
2024-07-23 18:51:36 +0900

    Support mini wallet

--- src/pages/library/external.tsx
+++ src/pages/library/external.tsx
@@ -1,45 +1,58 @@
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
-import { WagmiProvider, createConfig, http } from 'wagmi'
-import { Web3ModalButtons } from '../../components/Web3ModalButtons'
-import { WagmiTests } from '../../components/Wagmi/WagmiTests'
-import { ThemeStore } from '../../utils/StoreUtil'
-import { ConstantsUtil } from '../../utils/ConstantsUtil'
-import { WagmiModalInfo } from '../../components/Wagmi/WagmiModalInfo'
-import { mainnet } from 'viem/chains'
-import { externalTestConnector } from '../../utils/ConnectorUtil'
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { Web3ModalButtons } from "../../components/Web3ModalButtons";
+import { WagmiTests } from "../../components/Wagmi/WagmiTests";
+import { ThemeStore } from "../../utils/StoreUtil";
+import { ConstantsUtil } from "../../utils/ConstantsUtil";
+import { WagmiModalInfo } from "../../components/Wagmi/WagmiModalInfo";
+import { mainnet } from "viem/chains";
+import { walletConnect } from "../../utils/ConnectorUtil";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-const connectors = [externalTestConnector()]
+const connectors = [
+  walletConnect({
+    projectId: ConstantsUtil.ProjectId,
+    metadata: {
+      name: "WalletConnect App Demo",
+      description: "WalletConnect App Demo",
+      url: "https://walletconnect-app-demo.vercel.app",
+      icons: [
+        "https://developers.line.biz/assets/img/icons/docs/light/hover/liff.svg",
+      ],
+    },
+    showQrModal: false,
+  }),
+];
 
 const wagmiConfig = createConfig({
   chains: [mainnet],
   connectors,
   transports: {
-    1: http()
+    1: http(),
   },
-  multiInjectedProviderDiscovery: false
-})
+  multiInjectedProviderDiscovery: false,
+});
 
 const modal = createWeb3Modal({
   wagmiConfig,
   projectId: ConstantsUtil.ProjectId,
   enableAnalytics: true,
   metadata: ConstantsUtil.Metadata,
-  termsConditionsUrl: 'https://walletconnect.com/terms',
-  privacyPolicyUrl: 'https://walletconnect.com/privacy'
-})
+  termsConditionsUrl: "https://walletconnect.com/terms",
+  privacyPolicyUrl: "https://walletconnect.com/privacy",
+});
 
-ThemeStore.setModal(modal)
+ThemeStore.setModal(modal);
 
 export default function Wagmi() {
-  const [ready, setReady] = useState(false)
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    setReady(true)
-  }, [])
+    setReady(true);
+  }, []);
 
   return ready ? (
     <WagmiProvider config={wagmiConfig}>
@@ -49,5 +62,5 @@ export default function Wagmi() {
         <WagmiTests />
       </QueryClientProvider>
     </WagmiProvider>
-  ) : null
+  ) : null;
 }
