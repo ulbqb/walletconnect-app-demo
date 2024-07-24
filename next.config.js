150fd1726aae01e761f90b8dd8ce40e37fb4128a
Shogo Hyodo <mmoshg8u@gmail.com>
2024-07-24 14:00:42 +0900

    Remove unused thing

--- next.config.js
+++ next.config.js
@@ -2,20 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  distDir: 'out',
-  cleanDistDir: true,
   async headers() {
     return [
       {
-        source: '/.well-known/apple-app-site-association',
-        headers: [{ key: 'content-type', value: 'application/json' }]
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "content-type", value: "application/json" }],
       },
       {
-        source: '/.well-known/assetlinks.json',
-        headers: [{ key: 'content-type', value: 'application/json' }]
-      }
-    ]
-  }
-}
+        source: "/.well-known/assetlinks.json",
+        headers: [{ key: "content-type", value: "application/json" }],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
